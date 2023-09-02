8e62b540d321096e4a1cda2aadd168a915ba21c5
mehradmrt <mehradmortazavi@gmail.com>
2023-09-02 09:50:30 -0700

    Gazebo sims are added

---
+++ agbot/robot_simulator/launch/myrobot_state_publisher.launch.py
@@ -0,0 +1,35 @@
+import os
+from ament_index_python.packages import get_package_share_directory
+from launch import LaunchDescription
+from launch.actions import DeclareLaunchArgument
+from launch.substitutions import LaunchConfiguration, Command
+from launch_ros.actions import Node
+
+def generate_launch_description():
+    use_sim_time = LaunchConfiguration('use_sim_time')
+    urdf_file_name = 'robot_description_gazebo.urdf.xacro'
+    # urdf_file_name = 'waffle.urdf'
+    # urdf_file_name = 'sam_bot_description.urdf'
+    urdf_path = os.path.join(get_package_share_directory('robot_simulator'),'urdf_sim', urdf_file_name)
+
+    robot_desc = Command(['xacro ', urdf_path])
+    # with open(urdf_path, 'r') as infp:
+    #     robot_desc = infp.read()
+    
+    remappings = [('/tf', 'tf'), ('/tf_static', 'tf_static')]
+    
+    return LaunchDescription([
+
+        DeclareLaunchArgument(
+            'use_sim_time',
+            default_value='true',
+            description='Use simulation (Gazebo) clock if true'),
+
+        Node(
+            package='robot_state_publisher',
+            executable='robot_state_publisher',
+            parameters=[{'use_sim_time': use_sim_time, 'robot_description': robot_desc}],
+            remappings=remappings,
+            arguments=[urdf_path]
+            )
+    ])
--- agbot/robot_simulator/launch/simulation.launch.py
+++ agbot/robot_simulator/launch/simulation.launch.py
@@ -1,75 +1,78 @@
 import os
 from ament_index_python.packages import get_package_share_directory
 from launch import LaunchDescription
+from launch.conditions import IfCondition
 from launch.actions import IncludeLaunchDescription, DeclareLaunchArgument
 from launch.launch_description_sources import PythonLaunchDescriptionSource
-from launch.substitutions import LaunchConfiguration
+from launch.substitutions import LaunchConfiguration, PythonExpression
 from launch_ros.actions import Node
 
-
 def generate_launch_description():
-    urdf_path = os.path.join(get_package_share_directory('robot_simulator'),'urdf_sim','robot_description_gazebo.urdf.xacro')
+    # urdf_path = os.path.join(get_package_share_directory('robot_simulator'),'urdf_sim','robot_description_gazebo.urdf.xacro')
+    # model_path = os.path.join(get_package_share_directory('robot_simulator'),'models','my_robot','waffle.sdf')
     world_path = os.path.join(get_package_share_directory('robot_simulator'),'worlds','orchard.world')
-
+    ekf_config_path = os.path.join(get_package_share_directory('robot_simulator'),'config','ekf.yaml')
+    state_pub_path = os.path.join(get_package_share_directory('robot_simulator'),'launch','myrobot_state_publisher.launch.py')
     pkg_gazebo_ros = get_package_share_directory('gazebo_ros')
-    pkg_robot_state = get_package_share_directory('robot_state_publisher')
+    # robot_desc = Command(['xacro ', urdf_path]) 
     use_sim_time = LaunchConfiguration('use_sim_time', default='true')
+    x_pose = LaunchConfiguration('x_pose', default='27.0') 
+    y_pose = LaunchConfiguration('y_pose', default='-21.0')
+    Y_pose = LaunchConfiguration('Y_pose', default='1.5707')
 
-    # x_pose = LaunchConfiguration('x_pose', default='27.0')
-    # y_pose = LaunchConfiguration('y_pose', default='-21.0')
-    x_pose = LaunchConfiguration('x_pose', default='0.0')
-    y_pose = LaunchConfiguration('y_pose', default='0.0')
-    declare_x_position_cmd = DeclareLaunchArgument(
-        'x_pose', default_value='0.0',
-        description='Specify namespace of the robot')
 
-    declare_y_position_cmd = DeclareLaunchArgument(
-        'y_pose', default_value='0.0',
-        description='Specify namespace of the robot')
 
+    robot_state_publisher_cmd = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource(state_pub_path),
+        launch_arguments={'use_sim_time': use_sim_time}.items()
+    )
 
     gzserver_cmd = IncludeLaunchDescription(
         PythonLaunchDescriptionSource(
             os.path.join(pkg_gazebo_ros, 'launch', 'gzserver.launch.py')
         ),
-        launch_arguments={'world': world_path}.items()
+        launch_arguments={'world': world_path}.items(),
     )
 
     gzclient_cmd = IncludeLaunchDescription(
         PythonLaunchDescriptionSource(
             os.path.join(pkg_gazebo_ros, 'launch', 'gzclient.launch.py')
-        )
+        ),
+        condition=IfCondition(PythonExpression(['True', ' and not ', 'False']))
     )
 
-    robot_state_publisher_cmd = Node(
-        package='robot_state_publisher',
-        executable='robot_state_publisher',
-        arguments=[urdf_path],
-        parameters=[{'use_sim_time': use_sim_time}],
-        output='screen',
-    )
-    
     gazebo_spawner_cmd = Node(
         package='gazebo_ros',
         executable='spawn_entity.py',
         arguments=[
             '-entity', 'my_robot',
-            '-file', urdf_path,
+            # '-file', model_path,
             '-x', x_pose,
             '-y', y_pose,
-            '-z', '0.01'
+            '-z','0.0',
+            '-Y', Y_pose,
+            '-topic', 'robot_description'
         ],
         output='screen',
+    )
+
+    robot_localization_node = Node(
+         package='robot_localization',
+         executable='ekf_node',
+         name='ekf_filter_node',
+         output='screen',
+         parameters=[ekf_config_path, {'use_sim_time': use_sim_time}],
+         remappings=[('/set_pose', '/initialpose')]
     )
 
     ld = LaunchDescription()
 
     # Add the commands to the launch description
+    ld.add_action(robot_state_publisher_cmd)
     ld.add_action(gzserver_cmd)
     ld.add_action(gzclient_cmd)
-    # ld.add_action(robot_state_publisher_cmd)
-    ld.add_action(declare_x_position_cmd)
-    ld.add_action(declare_y_position_cmd)
-    # ld.add_action(gazebo_spawner_cmd)
+    ld.add_action(gazebo_spawner_cmd)
+    ld.add_action(robot_localization_node)
+    
 
     return ld

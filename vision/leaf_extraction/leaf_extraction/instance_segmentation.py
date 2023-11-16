fb236ebc15b5e1dbecdad1f21a8163903641c1a4
mehradmrt <mehradmortazavi@gmail.com>
2023-11-15 17:50:46 -0800

    masks are successfully located on xyz-rgb

--- vision/leaf_extraction/leaf_extraction/instance_segmentation.py
+++ vision/leaf_extraction/leaf_extraction/instance_segmentation.py
@@ -1,103 +1,91 @@
-import cv2
 import numpy as np
-import matplotlib.pyplot as plt
 import open3d as o3d
-
+import struct
+import cv2
+from PIL import Image
 import rclpy
 from rclpy.node import Node
-from sensor_msgs.msg import Image, PointCloud2
+from sensor_msgs.msg import PointCloud2
 from cv_bridge import CvBridge
 from ultralytics import YOLO
-import sensor_msgs_py.point_cloud2 as pc2
 
 class ImageProcessor(Node):
     def __init__(self):
         super().__init__('instance_segmentation')
-        self.rgb_subscription = self.create_subscription(
-            Image, '/camera/color/image_raw', self.rgb_callback, 10)
         self.point_cloud_subscription = self.create_subscription(
             PointCloud2, '/camera/depth/color/points', self.point_cloud_callback, 10)
         self.bridge = CvBridge()
         self.model = YOLO('/home/arya/instance_segmentation/best.pt')
-        self.rgb_image = None
+        self.colors = None
+        self.points = None
         self.point_cloud = None
         self.mask = None
         self.processed = False
 
-    def rgb_callback(self, msg):
-        try:
-            self.rgb_image = self.bridge.imgmsg_to_cv2(msg, "rgb8")
-            bgr_image = cv2.cvtColor(self.rgb_image, cv2.COLOR_RGB2BGR)
-            cv2.imwrite("/tmp/new_rgb.jpg", bgr_image)
-        except Exception as e:
-            self.get_logger().error(f"Error processing RGB image: {e}")
-            return
-
-        results = self.model(['/tmp/new_rgb.jpg'], imgsz=1280, save=True)
-        for result in results:
-            num = 3
-            best_mask = result.masks.masks[num]
-            best_mask_np = best_mask.cpu().numpy()
-            self.mask = cv2.resize(best_mask_np, (1280, 720), interpolation=cv2.INTER_NEAREST)
-            print(self.mask.shape)
-
     def point_cloud_callback(self, msg):
-        if self.processed or self.mask is None:
+        if self.processed:
             return
 
-        points = np.array(list(pc2.read_points(msg, field_names=("x", "y", "z"), skip_nans=False)))
-        
-        # Extract XYZ values
-        xyz = points[:, :3]
+        self.points, self.colors = self.extract_points_and_colors(msg)
+        self.mask = self.extract_masks()
 
-        # Reshape XYZ to match the mask's shape
-        xyz_reshaped = xyz.reshape((720, 1280, 3))
-        rgb_image_reshaped = cv2.cvtColor(self.rgb_image, cv2.COLOR_BGR2RGB).reshape((720, 1280, 3))
+        cloud = o3d.geometry.PointCloud()
+        cloud.points = o3d.utility.Vector3dVector(self.points)
+        cloud.colors = o3d.utility.Vector3dVector(self.colors / 255.0) 
 
-        square_size = 300
-
-        start_row = xyz_reshaped.shape[0] - square_size  # Starting row index
-        start_col = xyz_reshaped.shape[1] - square_size  # Starting column index
-        bottom_right_square = xyz_reshaped[start_row:, start_col:]
-        bottom_right_square_points = bottom_right_square.reshape(-1, 3)
-        square_cloud = o3d.geometry.PointCloud()
-        square_cloud.points = o3d.utility.Vector3dVector(bottom_right_square_points)
-        square_cloud.paint_uniform_color([0, 0, 0])  # Red color for the square points
-
-        # top_left_square = xyz_reshaped[0:square_size, 0:square_size]
-        # top_left_square_points = top_left_square.reshape(-1, 3)
-        # print("Shape of xyz_reshaped:", xyz_reshaped.shape)
-        # print("Shape of top_left_square:", top_left_square.shape)
-        # print("Shape of top_left_square_points:", top_left_square_points.shape)
-
-        # square_cloud = o3d.geometry.PointCloud()
-        # square_cloud.points = o3d.utility.Vector3dVector(top_left_square_points)
-        # square_cloud.paint_uniform_color([0, 0, 0])  # Red color for the square points
-
-        # Create a full point cloud
-        full_cloud = o3d.geometry.PointCloud()
-        full_cloud.points = o3d.utility.Vector3dVector(xyz)
-        # full_cloud.colors = o3d.utility.Vector3dVector(rgb_image_reshaped.reshape(-1, 3) / 255.0)
-        full_cloud.paint_uniform_color([0.5, 0.5, 0.5])
-
-        # Filtered points (using the mask)
+        xyz_reshaped = self.points.reshape((720, 1280, 3))
         filtered_xyz = xyz_reshaped[self.mask > 0]
         filtered_cloud = o3d.geometry.PointCloud()
         filtered_cloud.points = o3d.utility.Vector3dVector(filtered_xyz)
-        filtered_cloud.paint_uniform_color([0, 0, 0])  # Black color for filtered points
+        filtered_cloud.paint_uniform_color([1, 0, 0]) 
 
-        # Visualization
         coordinate_frame = o3d.geometry.TriangleMesh.create_coordinate_frame(size=0.1, origin=[0,0,0])
         vis = o3d.visualization.Visualizer()
         vis.create_window()
-        vis.add_geometry(full_cloud)
+        vis.add_geometry(cloud)
         vis.add_geometry(filtered_cloud)
-        vis.add_geometry(square_cloud)
         vis.add_geometry(coordinate_frame)
         vis.run()
         vis.destroy_window()
 
         self.processed = True
+
+    def extract_masks(self):
+        height, width = 720, 1280 
+        image_array = self.colors.reshape((height, width, 3)).astype(np.uint8)
+        image = Image.fromarray(image_array, 'RGB')
+
+        open_cv_image = cv2.cvtColor(np.array(image), cv2.COLOR_RGB2BGR)
+        cv2.imwrite("/home/arya/AgBot_reps/UCM-AgBot-ROS2/src/vision/leaf_extraction/segmentation_model/pc_rgb.jpg", open_cv_image)
+        results = self.model([open_cv_image], imgsz=1280, save=True)
+
+        combined_mask = np.zeros((height, width), dtype=np.uint8)
+
+        for result in results:
+            for num in range(0,2):  # Combines masks for num 0, 1, and 2
+                if num < len(result.masks.masks):
+                    best_mask = result.masks.masks[num]
+                    best_mask_np = best_mask.cpu().numpy()
+                    resized_mask = cv2.resize(best_mask_np, (width, height), interpolation=cv2.INTER_NEAREST)
+                    combined_mask = np.logical_or(combined_mask, resized_mask > 0).astype(np.uint8)
+
+        return combined_mask
+
+    def extract_points_and_colors(self, cloud_msg):
+        cloud_data = np.frombuffer(cloud_msg.data, dtype=np.uint8)
+        points = np.zeros((cloud_msg.width * cloud_msg.height, 3), dtype=np.float32)
+        colors = np.zeros((cloud_msg.width * cloud_msg.height, 3), dtype=np.uint8)
+
+        for i in range(cloud_msg.width * cloud_msg.height):
+            point_start = i * cloud_msg.point_step
+            x, y, z = struct.unpack_from('fff', cloud_data, offset=point_start)
+            points[i] = [x, y, z]
+
+            rgb_packed = struct.unpack_from('I', cloud_data, offset=point_start + 16)[0]
+            colors[i] = [(rgb_packed >> 16) & 0xFF, (rgb_packed >> 8) & 0xFF, rgb_packed & 0xFF]
+
+        return points, colors
+
 
 
 def main(args=None):

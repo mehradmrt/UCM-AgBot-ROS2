90823429107f9103644dda7940901aaef46ed6e2
mehradmrt <mehradmortazavi@gmail.com>
2023-11-16 13:17:20 -0800

    xyz points and middle points are extracted

--- vision/leaf_extraction/leaf_extraction/instance_segmentation.py
+++ vision/leaf_extraction/leaf_extraction/instance_segmentation.py
@@ -3,23 +3,34 @@ import open3d as o3d
 import struct
 import cv2
 from PIL import Image
+import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D
+
 import rclpy
 from rclpy.node import Node
 from sensor_msgs.msg import PointCloud2
 from cv_bridge import CvBridge
 from ultralytics import YOLO
 
+
 class ImageProcessor(Node):
     def __init__(self):
         super().__init__('instance_segmentation')
         self.point_cloud_subscription = self.create_subscription(
             PointCloud2, '/camera/depth/color/points', self.point_cloud_callback, 10)
+
         self.bridge = CvBridge()
         self.model = YOLO('/home/arya/instance_segmentation/best.pt')
+        self.conf_cutoff = 0.7
+
         self.colors = None
         self.points = None
         self.point_cloud = None
-        self.mask = None
+
+        self.combined_masks = None
+        self.ordered_masks = None
+        self.confs = None
+
         self.processed = False
 
     def point_cloud_callback(self, msg):
@@ -27,14 +38,21 @@ class ImageProcessor(Node):
             return
 
         self.points, self.colors = self.extract_points_and_colors(msg)
-        self.mask = self.extract_masks()
+        self.combined_masks, self.ordered_masks, self.confs = self.extract_masks()
+        self.masks_xyzs = self.extract_masks_xyzs(self.ordered_masks, self.points)
 
+        # self.plotting_o3d()
+        self.plot_masked_points_with_centers(5)
+
+        self.processed = True
+
+    def plotting_o3d(self):
         cloud = o3d.geometry.PointCloud()
         cloud.points = o3d.utility.Vector3dVector(self.points)
         cloud.colors = o3d.utility.Vector3dVector(self.colors / 255.0) 
 
         xyz_reshaped = self.points.reshape((720, 1280, 3))
-        filtered_xyz = xyz_reshaped[self.mask > 0]
+        filtered_xyz = xyz_reshaped[self.combined_masks.astype(bool)]
         filtered_cloud = o3d.geometry.PointCloud()
         filtered_cloud.points = o3d.utility.Vector3dVector(filtered_xyz)
         filtered_cloud.paint_uniform_color([1, 0, 0]) 
@@ -47,29 +65,7 @@ class ImageProcessor(Node):
         vis.add_geometry(coordinate_frame)
         vis.run()
         vis.destroy_window()
-
-        self.processed = True
-
-    def extract_masks(self):
-        height, width = 720, 1280 
-        image_array = self.colors.reshape((height, width, 3)).astype(np.uint8)
-        image = Image.fromarray(image_array, 'RGB')
-
-        open_cv_image = cv2.cvtColor(np.array(image), cv2.COLOR_RGB2BGR)
-        cv2.imwrite("/home/arya/AgBot_reps/UCM-AgBot-ROS2/src/vision/leaf_extraction/segmentation_model/pc_rgb.jpg", open_cv_image)
-        results = self.model([open_cv_image], imgsz=1280, save=True)
-
-        combined_mask = np.zeros((height, width), dtype=np.uint8)
-
-        for result in results:
-            for num in range(0,2):  # Combines masks for num 0, 1, and 2
-                if num < len(result.masks.masks):
-                    best_mask = result.masks.masks[num]
-                    best_mask_np = best_mask.cpu().numpy()
-                    resized_mask = cv2.resize(best_mask_np, (width, height), interpolation=cv2.INTER_NEAREST)
-                    combined_mask = np.logical_or(combined_mask, resized_mask > 0).astype(np.uint8)
-
-        return combined_mask
+        
 
     def extract_points_and_colors(self, cloud_msg):
         cloud_data = np.frombuffer(cloud_msg.data, dtype=np.uint8)
@@ -86,7 +82,63 @@ class ImageProcessor(Node):
 
         return points, colors
 
+    def extract_masks(self):
+        height, width = 720, 1280 
+        image_array = self.colors.reshape((height, width, 3)).astype(np.uint8)
+        image = Image.fromarray(image_array, 'RGB')
 
+        open_cv_image = cv2.cvtColor(np.array(image), cv2.COLOR_RGB2BGR)
+        cv2.imwrite("/home/arya/AgBot_reps/UCM-AgBot-ROS2/src/vision/leaf_extraction/segmentation_model/pc_rgb.jpg", open_cv_image)
+        results = self.model([open_cv_image], imgsz=1280, save=True, conf=self.conf_cutoff)
+
+        combined_masks = np.zeros((height, width), dtype=np.uint8)
+        ordered_masks = []
+
+        for result in results:
+            confs = result.boxes.conf.numpy()
+            for i in range(result.__len__()):  
+                mask_i = result.masks.masks[i].numpy()
+                resized_mask = cv2.resize(mask_i, (width, height), interpolation=cv2.INTER_NEAREST)
+                ordered_masks.append(resized_mask)
+                combined_masks = np.logical_or(combined_masks, resized_mask).astype(np.uint8)
+
+        return combined_masks, ordered_masks, confs
+
+    def extract_masks_xyzs(self, masks, points):
+        masks_xyzs = []
+        xyz_reshaped = points.reshape((720, 1280, 3))
+
+        for mask in masks:
+            mask_boolean = mask.astype(bool)
+            masked_points = xyz_reshaped[mask_boolean]
+            masks_xyzs.append(masked_points)
+
+        return masks_xyzs
+
+    def plot_masked_points_with_centers(self, top_n):
+        num_masks = min(len(self.masks_xyzs), top_n)
+        fig = plt.figure()
+        ax = fig.add_subplot(111, projection='3d')
+        colors = plt.cm.get_cmap('hsv', num_masks)
+
+        for i in range(num_masks):
+            points = self.masks_xyzs[i]
+            if points.size == 0:
+                continue
+
+            median_point = np.median(points, axis=0)
+            distances = np.linalg.norm(points - median_point, axis=1)
+            closest_point_index = np.argmin(distances)
+            central_point = points[closest_point_index]
+
+            ax.scatter(points[:, 0], points[:, 1], points[:, 2], color=colors(i), s=1)
+            ax.scatter(central_point[0], central_point[1], central_point[2], color='black', s=60)
+
+        ax.set_xlabel('X')
+        ax.set_ylabel('Y')
+        ax.set_zlabel('Z')
+        ax.view_init(elev=10, azim=120)
+        plt.show()
 
 def main(args=None):
     rclpy.init(args=args)
